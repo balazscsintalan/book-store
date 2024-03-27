0fea148cec0157faeb0720d1f195be2a5f3e4a46
balazs <csintalan.balazs@danubiusinfo.hu>
2024-03-27 13:02:14 +0100

    initial commit

---
+++ src/test/java/hu/danubius/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.danubius.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
